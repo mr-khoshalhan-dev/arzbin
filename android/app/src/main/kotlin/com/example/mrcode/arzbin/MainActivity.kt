ac0e19482f82e704bee3c42bdc93cffd1d327347
mr-khoshalhan-dev <marcode67yo@gmail.com>
2025-04-25 16:48:20 -0700

    start pro arzbin

---
+++ android/app/src/main/kotlin/com/example/mrcode/arzbin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mrcode.arzbin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'Screen/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home_Screen(),
+    );
+  }
+}
