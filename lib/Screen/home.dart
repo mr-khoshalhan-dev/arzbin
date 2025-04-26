181534d88c7a91ead3f1df34663e84477d737ef5
mr-khoshalhan-dev <marcode67yo@gmail.com>
2025-04-25 17:06:07 -0700

    start pro arzbin

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -10,9 +10,21 @@ class _Home_ScreenState extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white38,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Row(
+        // ====================appbar new=======================
+         children: [
+          Text("arzbin",style: TextStyle(fontSize: 18,fontWeight:FontWeight.bold)),
 
+           Icon(Icons.menu),
+         ],
+
+
+        //   ======================exit============================
+
+        //
         ),
       ),
 
